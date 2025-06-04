a29b1599b71fd4d1700bfac97b0617a540448eb9
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-06-03 22:26:53 -0400

    Add ViewModel for ProductDetail

---
+++ DevStore/ViewModels/ProductDetailViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDetailViewModel.swift
+//  DevStore
+//
+//  Created by Bonmyeong Koo - Vendor on 6/3/25.
+//
+
+import Foundation
+
+@MainActor
+final class ProductDetailViewModel: ObservableObject {
+    @Published var product: Product?
+
+    init(product: Product) {
+        self.product = product
+    }
+}
