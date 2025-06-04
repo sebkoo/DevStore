12e9c2cb0399aa4b00b0e40772edf7a72b86171e
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-06-04 10:48:28 -0400

    Add CheckoutSession model with URL

---
+++ DevStore/Models/CheckoutSession.swift
@@ -0,0 +1,12 @@
+//
+//  CheckoutSession.swift
+//  DevStore
+//
+//  Created by Bonmyeong Koo - Vendor on 6/4/25.
+//
+
+import Foundation
+
+struct CheckoutSession: Codable {
+    let checkoutUrl: String
+}
