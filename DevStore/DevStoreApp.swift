06705809feb472a23346db785b1230c5fbed3588
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-06-04 10:22:09 -0400

    Inject CartManager globally

--- DevStore/DevStoreApp.swift
+++ DevStore/DevStoreApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct DevStoreApp: App {
+    @StateObject private var cartManager = CartManager()
+
     var body: some Scene {
         WindowGroup {
-
+            ProductListView(viewModel: ProductListViewModel(service: DefaultProductService()))
+                .environmentObject(cartManager)
         }
     }
 }
